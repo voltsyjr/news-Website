b1fe30b6cd2c149d2a18c3af1fb4decbad6b9923
Vishal <vishalsingh2508720@gmail.com>
2021-06-08 00:24:04 +0530

    commit using gitbash

--- config.php
+++ config.php
@@ -2,4 +2,5 @@
 $conn = mysqli_connect("localhost","root","","news-site")or die("Coneection failed: ". mysqli_connect_error());
 $hostname='https://localhost/news-site';
 // this change is made in develope
+// ?another commit
 ?>
